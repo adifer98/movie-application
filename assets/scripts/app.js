e0e8a7f4d040c76235aa8cfecaa19f4debc33f3a
Adifer98 <adifermon@campus.technion.ac.il>
2024-01-10 17:35:23 +0200

    add button and cancel button listeners

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,2 +1,23 @@
+const headerElement = document.querySelector("header");
+const addMovieBtn = headerElement.querySelector('button');
+const addModalElement = document.getElementById("add-modal");
+const backdropEl = document.getElementById('backdrop');
+const addModalCancelBtn = addModalElement.querySelector(".btn--passive");
 
-document.getElement.....
+
+const visualizeBackdrop = () => {
+    backdropEl.classList.toggle("visible");
+    };
+
+const visualizeAddModal = () => {
+    addModalElement.classList.toggle('visible');
+    visualizeBackdrop();
+    };
+
+const unVisualizeAddModal = () => visualizeAddModal();
+
+addMovieBtn.addEventListener('click', visualizeAddModal);
+
+addModalCancelBtn.addEventListener('click', unVisualizeAddModal);
+
+
